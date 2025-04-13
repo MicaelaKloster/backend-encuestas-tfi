49cf5adb59df4f67aa613842cdf64a8f0a9794b7
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2025-04-13 18:02:10 -0300

    dependencias instaladas, documentación creada y creación de encuestas con sus respectivos archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { EncuestasModule } from './modules/encuestas/encuestas.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    EncuestasModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+  ],
 })
 export class AppModule {}
